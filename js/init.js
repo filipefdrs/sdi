4856e1e6a62313b51f65b9e388892ccbd437b1b6
Filipe Reis <38828226+filipefdrs@users.noreply.github.com>
2020-12-23 07:42:18 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown({hover: false});
     $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+    setInterval(function() {$('.carousel').carousel('next');}, 3000);
     $('.modal').modal();
 
   }); // end of document ready
